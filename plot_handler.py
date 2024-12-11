456a7c0c223ffca0d20a947e9d489c7b7d503a85
manuelcurnis <manuel.curnis@epfl.ch>
2024-12-11 17:45:17 +0100

    clean code

--- plot_handler.py
+++ plot_handler.py
@@ -16,8 +16,8 @@ def plot(ax, train_loss_history, val_loss_history, num_epochs):
 
     t = np.arange(1, num_epochs + 1)
 
-    ax.semilogy(t, train_loss_history, label="Train loss")
-    ax.semilogy(t, val_loss_history, label="Val loss")
+    ax.semilogy(t, train_loss_history, label="Train loss", linewidth=1)
+    ax.semilogy(t, val_loss_history, label="Val loss", linewidth=1)
     ax.legend()
     ax.set_xlabel("Epoch")
     ax.set_ylabel("Loss")
